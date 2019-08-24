4881a75fde29e337affd4d82df85249d33d9d712
md678685 <1917406+md678685@users.noreply.github.com>
2019-08-24 14:20:52 +0100

    Add several missing compound aliases

--- src/main/java/io/github/essentialsx/itemdbgenerator/providers/alias/WoodAliasProvider.java
+++ src/main/java/io/github/essentialsx/itemdbgenerator/providers/alias/WoodAliasProvider.java
@@ -32,11 +32,11 @@ public class WoodAliasProvider extends CompoundAliasProvider {
     @SuppressWarnings("unused")
     private enum WoodType {
         ACACIA("ac", "a"),
-        BIRCH("b"),
-        DARK_OAK("darkoak", "dark", "do"),
-        JUNGLE("j"),
+        BIRCH("b", "light", "l", "white", "w"),
+        DARK_OAK("dark", "do"),
+        JUNGLE("j", "forest", "f"),
         OAK("o"),
-        SPRUCE("sp", "s");
+        SPRUCE("pine", "p", "dark", "d", "s");
 
         private final String[] names;
 
@@ -61,23 +61,23 @@ public class WoodAliasProvider extends CompoundAliasProvider {
      */
     @SuppressWarnings("unused")
     private enum WoodItemType {
-        BOAT(null, "boat%s", "%sboat"),
+        BOAT(null, "boat%s", "%sboat", "%sraft"),
         BUTTON(null, "button%s", "%sbutton"),
         DOOR("[A-Z_]+_DOOR"),
         FENCE("[A-Z_]+_FENCE$"),
         FENCE_GATE(null, "%sgate", "%sfencegate", "gate%s"),
-        LEAVES(null, "%sleaves", "%sleaf", "leaves%s", "leaf%s"),
-        LOG("^[A-Z_]+_LOG", "log%s", "%slog"),
-        PLANKS(null),
-        PRESSURE_PLATE(null, "%spplate", "%spressureplate", "%splate", "plate%s"),
-        SAPLING("^[A-Z_]+_SAPLING"),
-        SLAB(null, "%sslab", "$shalfblock"),
-        STAIRS(null, "%sstairs", "%sstair"),
-        TRAPDOOR(null, "%strapdoor", "%stdoor"),
-        WOOD("^[A-Z_]+_WOOD", "%swood", "wood%s"),
+        LEAVES(null, "%sleaves", "%sleaf", "leaves%s", "leaf%s", "%streeleaves", "%slogleaves", "%strunkleaves", "%swoodleaves", "%streeleaf", "%slogleaf", "%strunkleaf", "%swoodleaf", "%sleaf", "%streeleave", "%slogleave", "%strunkleave", "%swoodleave", "%sleave"),
+        LOG("^[A-Z_]+_LOG", "log%s", "%slog", "%strunk", "%stree"),
+        PLANKS(null, "%swoodenplank", "%swoodplank", "%swplank", "%splankwooden", "%splankwood", "%splankw", "%splank"),
+        PRESSURE_PLATE(null, "%spplate", "%spressureplate", "%splate", "plate%s", "%spressplate"),
+        SAPLING("^[A-Z_]+_SAPLING", "%ssapling", "%streesapling", "%slogsapling", "%strunksapling", "%swoodsapling"),
+        SLAB(null, "%swoodenstep", "%swoodstep", "%swstep", "%sstep", "%swoodenslab", "%swoodslab", "%swslab", "%swoodenhalfblock", "%swoodhalfblock", "%swhalfblock", "%shalfblock"),
+        STAIRS(null, "%swoodenstairs", "%swoodstairs", "%swstairs", "%swoodenstair", "%swoodstair", "%swstair", "%sstair"),
+        TRAPDOOR(null, "%strapdoor", "%sdoortrap", "%shatch", "%stdoor", "%sdoort", "%strapd", "%sdtrap"),
+        WOOD("^[A-Z_]+_WOOD", "%swood", "%slogall", "%strunkall", "%streeall", "wood%s"),
         POTTED_SAPLING("POTTED_[A-Z_]+_SAPLING", "%spot", "potted%s", "potted%ssapling"),
-        STRIPPED_LOG("STRIPPED_[A-Z_]+_LOG", "stripped%slog", "log%sstripped", "str%slog"),
-        STRIPPED_WOOD("STRIPPED_[A-Z_]+_WOOD", "stripped%swood", "wood%sstripped", "str%swood"),
+        STRIPPED_LOG("STRIPPED_[A-Z_]+_LOG", "stripped%slog", "log%sstripped", "str%slog", "%sstrippedlog", "%sbarelog", "stripped%stree", "bare%stree", "stripped%strunk", "bare%strunk"),
+        STRIPPED_WOOD("STRIPPED_[A-Z_]+_WOOD", "stripped%swood", "wood%sstripped", "str%swood", "%sstrippedwood", "%sbarewood", "stripped%slogall", "bare%slogall", "stripped%strunkall", "bare%strunkall", "stripped%streeall", "bare%streeall"),
         ;
 
         private final Pattern regex;
