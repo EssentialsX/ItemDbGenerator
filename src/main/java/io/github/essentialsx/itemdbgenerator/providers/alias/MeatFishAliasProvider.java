1d8fad7df97d05de8fdb35ae0e0257e878e65933
md678685 <1917406+md678685@users.noreply.github.com>
2020-06-23 19:17:42 +0100

    Fix rabbit stew overriding raw rabbit

--- src/main/java/io/github/essentialsx/itemdbgenerator/providers/alias/MeatFishAliasProvider.java
+++ src/main/java/io/github/essentialsx/itemdbgenerator/providers/alias/MeatFishAliasProvider.java
@@ -17,7 +17,7 @@ public class MeatFishAliasProvider extends CompoundAliasProvider {
         if (isBlacklisted(item.getMaterial())) return null;
 
         Food food = Food.of(item.getMaterial());
-        FoodModifier itemType = FoodModifier.of(item.getMaterial());
+        FoodType itemType = FoodType.of(item.getMaterial());
 
         if (food == null || itemType == null) {
             return null;
@@ -68,17 +68,17 @@ public class MeatFishAliasProvider extends CompoundAliasProvider {
         }
     }
 
-    // ...trust me this makes sense I promise
-    private enum FoodModifier implements CompoundType {
+    private enum FoodType implements CompoundType {
         COOKED("COOKED_[A-Z_]+", "%scooked", "%scook", "%sc", "%sgrilled", "%sgrill", "%sg", "%sroasted", "%sroast", "%sro", "%sbbq", "%stoasted", "cooked%s", "cook%s", "c%s", "grilled%s", "grill%s", "g%s", "roasted%s", "roast%s", "ro%s", "bbq%s", "toasted%s"),
         HIDE("[A-Z_]+_HIDE", "%shide", "%sskin", "%scoat", "%sfur"),
-        RAW("^(?!COOKED_)[A-Z_]+", "raw%s", "ra%s", "uncooked%s", "plain%s", "%s"),
+        STEW("[A-Z_]+_STEW", "%sstew", "%ssoup"),
+        RAW("^(?!COOKED_)[A-Z_]+(?!_STEW)$", "raw%s", "ra%s", "uncooked%s", "plain%s", "%s"),
         ;
 
         private final Pattern regex;
         private final String[] formats;
 
-        FoodModifier(String regex, String... formats) {
+        FoodType(String regex, String... formats) {
             this.regex = CompoundType.generatePattern(name(), regex);
             this.formats = CompoundType.generateFormats(name(), formats);
         }
@@ -88,10 +88,10 @@ public class MeatFishAliasProvider extends CompoundAliasProvider {
             return formats;
         }
 
-        public static FoodModifier of(Material material) {
+        public static FoodType of(Material material) {
             String matName = material.name();
 
-            for (FoodModifier type : values()) {
+            for (FoodType type : values()) {
                 if (type.regex.matcher(matName).matches()) {
                     return type;
                 }
