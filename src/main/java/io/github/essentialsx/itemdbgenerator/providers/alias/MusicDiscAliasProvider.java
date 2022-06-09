5096371195f586f92849b306ba6963892a6d2bcf
MD <1917406+mdcfe@users.noreply.github.com>
2022-06-09 17:57:15 +0100

    Update music discs

--- src/main/java/io/github/essentialsx/itemdbgenerator/providers/alias/MusicDiscAliasProvider.java
+++ src/main/java/io/github/essentialsx/itemdbgenerator/providers/alias/MusicDiscAliasProvider.java
@@ -10,44 +10,84 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class MusicDiscAliasProvider extends CompoundAliasProvider {
-    private static final List<String> MUSIC_DISC_NAMES = Arrays.asList("musicrecord", "musicdisk", "musicdisc", "musiccd", "mrecord", "mdisk", "mdisc", "mcd", "record", "disk", "disc", "cd");
-    // TODO: fragment
+    private static final List<String> MUSIC_DISC_NAMES = Arrays.asList("musicrecord", "musicdisk", "musicdisc", "mdisc", "record", "disk", "disc", "cd");
 
     @Override
     public Stream<String> get(ItemProvider.Item item) {
         if (!(item instanceof MaterialEnumProvider.MaterialEnumItem)) return null;
 
         Track track = Track.of(item.getMaterial());
-        if (track == null) return null;
+        DiscType type = DiscType.of(item.getMaterial());
+        if (track == null || type == null) return null;
 
-        return MUSIC_DISC_NAMES.stream()
-                .flatMap(track::format);
+        return getAliases(track, type);
     }
 
-    private enum Track implements CompoundType {
-        THIRTEEN("MUSIC_DISC_13", "13%s", "gold%s", "go%s", "%s1", "1%s"),
-        CAT("MUSIC_DISC_CAT", "cat%s", "green%s", "gr%s", "%s2", "2%s"),
-        BLOCKS("MUSIC_DISC_BLOCKS", "blocks%s", "orange%s", "or%s", "%s3", "3%s"),
-        CHIRP("MUSIC_DISC_CHIRP", "chirp%s", "red%s", "re%s", "%s4", "4%s"),
-        FAR("MUSIC_DISC_FAR", "far%s", "lightgreen%s", "lgreen%s", "lightgr%s", "lgr%s"), // previously 5, replaced by Music Disc 5 in 1.19
-        MALL("MUSIC_DISC_MALL", "mall%s", "purple%s", "pu%s", "%s6", "6%s"),
-        MELLOHI("MUSIC_DISC_MELLOHI", "mellohi%s", "pink%s", "pi%s", "%s7", "7%s"),
-        STAL("MUSIC_DISC_STAL", "stal%s", "black%s", "bl%s", "%s8", "8%s"),
-        STRAD("MUSIC_DISC_STRAD", "strad%s", "white%s", "wh%s", "%s9", "9%s"),
-        WARD("MUSIC_DISC_WARD", "ward%s", "darkgreen%s", "dgreen%s", "darkgr%s", "dgr%s", "%s10", "10%s"),
-        ELEVEN("MUSIC_DISC_11", "11%s", "cracked%s", "crack%s", "c%s", "%s11"),
-        WAIT("MUSIC_DISC_WAIT", "wait%s", "blue%s", "cyan%s", "bl%s", "cy%s", "%s12", "12%s"),
-        PIGSTEP("MUSIC_DISC_PIGSTEP", "pigstep%s", "nether%s", "dark%s", "neth%s", "pig%s", "%s14", "14%s", "lenaraineisawesome"),
-        OTHERSIDE("MUSIC_DISC_OTHERSIDE", "otherside%s", "cave%s", "under%s", "deep%s2", "other%s", "%s15", "15%s", "lenaraineisstillawesome"),
-        FIVE("DISC_(FRAGMENT_)?5", "five%s", "%sfive", "deepdark%s", "%sdeepdark", "deep%s", "%sdeep", "%s5", "5%s", "%s16", "16%s")
+    private enum DiscType implements CompoundType {
+        DISC_FRAGMENT(null, "discfrag", "fragment"),
+        MUSIC_DISC(null, MUSIC_DISC_NAMES)
         ;
 
         private final Pattern regex;
         private final String[] formats;
 
-        Track(String regex, String... formats) {
+        DiscType(String regex, String... formats) {
             this.regex = CompoundType.generatePattern(name(), regex);
             this.formats = CompoundType.generateFormats(name(), formats);
+        }
+
+        DiscType(String regex, List<String> names) {
+            this(regex, generateFormatsFromNames(names));
+        }
+
+        private static String[] generateFormatsFromNames(List<String> names) {
+            return names.stream()
+                    .flatMap(name -> Stream.of(name + "%s", "%s" + name))
+                    .toArray(String[]::new);
+        }
+
+        public static DiscType of(Material material) {
+            String matName = material.name();
+
+            for (DiscType type : values()) {
+                if (type.regex.matcher(matName).matches()) {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        public String[] getFormats() {
+            return formats;
+        }
+    }
+
+    private enum Track implements CompoundModifier {
+        THIRTEEN("MUSIC_DISC_13", "13", "gold", "go", "1"),
+        CAT("MUSIC_DISC_CAT", "cat", "green", "gr", "2"),
+        BLOCKS("MUSIC_DISC_BLOCKS", "blocks", "orange", "or", "3"),
+        CHIRP("MUSIC_DISC_CHIRP", "chirp", "red", "re", "4"),
+        FAR("MUSIC_DISC_FAR", "far", "lightgreen", "lgreen", "lightgr", "lgr"), // previously 5, replaced by Music Disc 5 in 1.19
+        MALL("MUSIC_DISC_MALL", "mall", "purple", "pu", "6"),
+        MELLOHI("MUSIC_DISC_MELLOHI", "mellohi", "pink", "pi", "7"),
+        STAL("MUSIC_DISC_STAL", "stal", "black", "bl", "8"),
+        STRAD("MUSIC_DISC_STRAD", "strad", "white", "wh", "9"),
+        WARD("MUSIC_DISC_WARD", "ward", "darkgreen", "dgreen", "darkgr", "dgr", "10"),
+        ELEVEN("MUSIC_DISC_11", "11", "cracked", "cr", "11"),
+        WAIT("MUSIC_DISC_WAIT", "wait", "blue", "cyan", "bl", "cy", "12"),
+        PIGSTEP("MUSIC_DISC_PIGSTEP", "pigstep", "nether", "dark", "neth", "pig", "14", "lenaraineisawesome"),
+        OTHERSIDE("MUSIC_DISC_OTHERSIDE", "otherside", "cave", "under", "deep", "other", "15", "lenaraineisstillawesome"),
+        FIVE("DISC_(FRAGMENT_)?5", "five", "wild", "5", "16")
+        ;
+
+        private final Pattern regex;
+        private final String[] names;
+
+        Track(String regex, String... names) {
+            this.regex = CompoundType.generatePattern(name(), regex);
+            this.names = CompoundType.generateFormats(name(), names);
         }
 
         public static Track of(Material material) {
@@ -63,8 +103,8 @@ public class MusicDiscAliasProvider extends CompoundAliasProvider {
         }
 
         @Override
-        public String[] getFormats() {
-            return formats;
+        public String[] getNames() {
+            return names;
         }
     }
 
