c5f72472bf3cbb593d72e5d623a8c91569a6e8c7
Josh Roy <10731363+JRoy@users.noreply.github.com>
2023-12-08 17:55:26 -0500

    Don't process experimental items
    use asm because md5 hates me

--- src/main/java/io/github/essentialsx/itemdbgenerator/providers/item/MaterialEnumProvider.java
+++ src/main/java/io/github/essentialsx/itemdbgenerator/providers/item/MaterialEnumProvider.java
@@ -1,8 +1,16 @@
 package io.github.essentialsx.itemdbgenerator.providers.item;
 
 import org.bukkit.Material;
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Opcodes;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 /**
@@ -11,8 +19,31 @@ import java.util.stream.Stream;
 public class MaterialEnumProvider implements ItemProvider {
     @Override
     public Stream<Item> get() {
-        return Arrays.stream(Material.values())
+      final Set<Material> experimentalMaterials = new HashSet<>();
+
+      try {
+        ClassReader cr = new ClassReader(Material.class.getName());
+        cr.accept(new ClassVisitor(Opcodes.ASM9) {
+          @Override
+          public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+            return new FieldVisitor(Opcodes.ASM9) {
+              @Override
+              public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
+                if (descriptor.equals("Lorg/bukkit/MinecraftExperimental;")) {
+                  experimentalMaterials.add(Material.valueOf(name));
+                }
+                return super.visitAnnotation(descriptor, visible);
+              }
+            };
+          }
+        }, 0);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+
+      return Arrays.stream(Material.values())
                 .filter(mat -> !mat.name().contains("LEGACY"))
+                .filter(mat -> !experimentalMaterials.contains(mat))
                 .filter(Material::isItem)
                 .map(MaterialEnumItem::new);
     }
